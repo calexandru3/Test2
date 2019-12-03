ae54578b6dcdc83176eb01414ff141fa76da6a66
Florin Precup <florin.precup@itsmartsystems.eu>
2019-12-03 11:05:48 +0200

    create project

---
+++ Test2/Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Bogdan Marica on 03/12/2019.
+//  Copyright © 2019 Florin Precup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
